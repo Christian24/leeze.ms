beca58e359cf3f0cb473db9160221fabb8926d07
Christian Siebmanns <christian.siebmanns@hotmail.de>
2015-12-07 13:37:58 +0100

    Added Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207120832) do
+ActiveRecord::Schema.define(version: 20151207121933) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tour_categories", force: :cascade do |t|
+    t.integer  "tour_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tour_categories", ["category_id"], name: "index_tour_categories_on_category_id"
+  add_index "tour_categories", ["tour_id"], name: "index_tour_categories_on_tour_id"
 
   create_table "tours", force: :cascade do |t|
     t.string   "title"
