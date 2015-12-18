096c7624643cfb1d92d8c45ea01c5af0306b20ee
Christian Siebmanns <christian.siebmanns@hotmail.de>
2015-12-18 14:11:25 +0100

    Added delete button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def button_to(name, url)
     link_to name, url, class: "btn btn-default"
   end
+  def button_to_delete(name, url)
+    link_to name, url, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn btn-default"
+  end
 end
