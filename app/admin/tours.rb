ecda0333a62f202b0b8d52ed8cae7424fbfcfa54
Christian Siebmanns <christian.siebmanns@hotmail.de>
2015-12-21 10:22:54 +0100

    Tours WIP admin

--- app/admin/tours.rb
+++ app/admin/tours.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Tour do
+permit_params :title, :teaser, :description, :url, category_ids: []
+  form do |f|
+    f.inputs "Details" do
+    f.input :title
+    f.input :teaser
+    f.input :description
+    f.input :url
+    f.input :categories
+    end
+    f.actions
+  end
 
 end
