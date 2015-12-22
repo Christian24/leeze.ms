d72c5ad31b0e0c32eff51b92b4356ac80f6f97b6
Christian Siebmanns <christian.siebmanns@hotmail.de>
2015-12-22 04:05:38 +0100

    Some light refactoring

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -17,13 +17,8 @@
             .where(table[:title].matches("%#{params[:search]}%"))
                      .where(tour_categories:{category_id: params[:category_id]})
                    .references(:tour_categories)
-       if params[:category_id].present?
-       #@tours = Tour.includes(:tour_categories)
-        #            .where(tour_categories:{category_id: params[:category_id]})
-         #           .references(:tour_categories)
-         #@tours = @tours.references(:tour_categories).where(tour_categories:{category_id: params[:category_id]})
-        end
-        #@tours = @tours.joins(:categories).where(:categories => params[:category_id])
+
+
         render :layout => false
 
 =begin
