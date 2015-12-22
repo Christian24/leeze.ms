aafe05f93cd532014af132baad4b02eb577b2d2d
Christian Siebmanns <christian.siebmanns@hotmail.de>
2015-12-22 02:29:38 +0100

    More fixes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,6 +3,17 @@
  */
 $(document).ready(function() {
     $('select').multiselect();
+    $("#category_id").change(function() {
+        var form = $("#live_search_form"); // grab the form wrapping the search bar.
+
+
+        var formData = form.serialize(); // grab the data in the form
+        var url = "/search";
+        $.get(url,formData, function(html){
+            //$("#live-search-results").html(html);
+            $("#live-search-results").html(html);
+        });
+    });
     $("#search").bind("keyup", function () {
         var form = $("#live_search_form"); // grab the form wrapping the search bar.
 
